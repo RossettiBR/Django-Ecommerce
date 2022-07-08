8099757d800fd32e3fe685b80f863e845d984fe2
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-08 11:28:43 -0300

    create variacao produto

--- produto/models.py
+++ produto/models.py
@@ -53,12 +53,12 @@ class Produto(models.Model):
         return self.nome
 
 
-"""        Variacao:
-            nome - char
-            produto - FK Produto
-            preco - Float
-            preco_promocional - Float
-            estoque - Int"""
-
 class Variacao(models.Model):
-    ...
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField()
+    preco_promocional = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
