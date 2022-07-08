1c285ceb2097fa710d50bfcdc6f77d3534ec0616
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-08 11:24:05 -0300

    create function image resize

--- produto/admin.py
+++ produto/admin.py
@@ -3,6 +3,9 @@ from .models import Produto
 
 
 class ProtudoAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'nome', 'preco_marketing', \
+        'preco_marketing_promocional', 'tipo',
+    list_display_links = 'id', 'nome',
 
-admin.site.register(Produto)
+
+admin.site.register(Produto, ProtudoAdmin)
