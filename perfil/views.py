98f8ecc9a4d66fd338910aa818287a02f8d1f563
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-11 11:29:45 -0300

    create ulrs & views perfil

--- perfil/views.py
+++ perfil/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import ListView
 
-# Create your views here.
+
+class Criar(View):
+    pass
+
+
+class Atualizar(View):
+    pass
+
+
+class Login(View):
+    pass
+
+
+class Logout(View):
+    pass
