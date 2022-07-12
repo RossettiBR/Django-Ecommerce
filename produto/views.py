0455a49d912198b4ffe6d06de0499b126c0ddb57
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-12 15:49:17 -0300

    create view detalhe

--- produto/views.py
+++ produto/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from .models import Produto
 
@@ -12,9 +13,11 @@ class ListaProdutos(ListView):
     paginate_by = 1
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe Produto')
+class DetalheProduto(DetailView):
+    model = Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 
 class AdicionarAoCarrinho(View):
--- utils/utils.py
+++ utils/utils.py
@@ -1,3 +1,2 @@
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
-
