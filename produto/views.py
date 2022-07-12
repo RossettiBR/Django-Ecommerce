361e2c648ad0d7478d6e66185ff42ba011e74fd2
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-12 15:11:49 -0300

    create pagination

--- produto/views.py
+++ produto/views.py
@@ -9,6 +9,7 @@ class ListaProdutos(ListView):
     model = Produto
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
+    paginate_by = 1
 
 
 class DetalheProduto(View):
