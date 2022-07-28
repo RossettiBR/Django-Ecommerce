049eed5a4ad90860e95f7f61b355614c0c74361a
Wanderson Rossetti <pastarossetti@gmail.com>
2022-07-28 10:51:18 -0300

    create def carrinho variacao_carrinho, variacao_id

--- pedido/views.py
+++ pedido/views.py
@@ -1,9 +1,9 @@
-from multiprocessing import context
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from django.views import View
 from django.contrib import messages
+from produto.models import Produto, Variacao
 
 
 class Pagar(View):
@@ -24,6 +24,27 @@ class Pagar(View):
             )
             return redirect('produto:lista')
 
+        carrinho = self.request.session.get('carrinho')
+        carrinho_variacao_ids = [v for v in carrinho]
+        bd_variacoes = list(
+            Variacao.objects.select_related('produto')
+            .filter(id__in=carrinho_variacao_ids)
+        )
+
+        for variacao in bd_variacoes:
+            vid = variacao.id
+
+            estoque = variacao.estoque
+            qtd_carrinho = carrinho[vid]['quantidade']
+            preco_unt = carrinho[vid]['preco_unitario']
+            preco_unit_promo = carrinho[vid]['preco_unitario_promocional']
+
+            if estoque < qtd_carrinho:
+                carrinho[vid]['quantidade'] = estoque
+                carrinho[vid]['preco_quantitativo'] = estoque * preco_unt
+                carrinho[vid]['preco_quantitativo_promocional'] = estoque * \
+                    preco_unit_promo
+                    
         contexto = {}
 
         return render(self.request, self.template_name, contexto)
