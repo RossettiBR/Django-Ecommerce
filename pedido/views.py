d53c8c2fd86bdef49a306224b88d7ae993152dcd
Wanderson Rossetti <pastarossetti@gmail.com>
2022-08-04 10:53:53 -0300

    create def get_queryset

--- pedido/views.py
+++ pedido/views.py
@@ -8,11 +8,25 @@ from . models import Pedido, ItemPedido
 from utils import utils
 
 
+class DispatchLoginRequired(View):
+    def dispatch(self, *args, **kwargs):
+        if not self.request.user.is_authenticated:
+            return redirect('perfil:criar')
+
+        return super().dispatch(*args, **kwargs)
+
+
 class Pagar(DetailView):
     template_name = 'pedido/pagar'
     model = Pedido
     pk_url_kwarg = 'pk'
     context_object_name = 'pedido'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(usuario=self.request.user)
+
+        return qs
 
 
 class SalvarPedido(View):
